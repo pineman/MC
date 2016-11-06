90d441f1d89b7aa5ed1b0d7e60ce2dac9849281a
joajfreitas <joaj.freitas@gmail.com>
2016-11-06 01:26:29 +0000

    Anexos e identificação do morais

--- src/metodo_newton.m
+++ src/metodo_newton.m
@@ -1,7 +1,7 @@
 function [ans, dif_ans] = metodo_newton(func, func_der, aprox, e)
     
     ans = aprox + func(aprox)/func_der(aprox);
-    %dif_ans é lambda_n+1 - lambda_n
+    %dif_ans e lambda_n+1 - lambda_n
     dif_ans = abs(ans(end) - aprox);
     while dif_ans(end) > e
         ans = [ans (ans(end) - func(ans(end))/func_der(ans(end)))];
